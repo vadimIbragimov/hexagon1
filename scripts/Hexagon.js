91cd64562b021714dd0eb0c677cbf413afb2ac1c
va ib <faldown@mail.ru>
2020-07-02 20:48:00 +0500

    add printing and all calculations

---
+++ scripts/Hexagon.js
@@ -0,0 +1,61 @@
+export class Hexagon {
+    constructor(x, y, size, canvas,paddingLeft,paddingTop) {
+        let lineWidth = 2;
+        this.size = size;
+        this.canvas = canvas;
+        this.value = false;
+        this.domen = undefined;
+        this.x = x;
+        this.y = y;
+        this.mainColor = '#000000';
+        let x0 = x * (size * Math.sin(Math.PI / 3) * 2) + (1 + y % 2) * size * Math.sin(Math.PI / 3) + paddingLeft;
+        let y0;
+        if (y > 0) {
+            y0 = y * (size + size * Math.cos(Math.PI / 3)) + paddingTop;
+        } else {
+            y0 = y * (size + size * Math.cos(Math.PI / 3) * 2) + paddingTop;
+        }
+        this.xc = x0;
+        this.yc = y0 + (size * Math.cos(Math.PI / 3)) + size / 2;
+        this.border = this.print(size, x0, y0, this.canvas, lineWidth);
+        this.fill = this.print(size, x0, y0, this.canvas, lineWidth, 'fill');
+        this.canvas.lineWidth = lineWidth;
+        this.canvas.strokeStyle = this.mainColor;
+        this.canvas.stroke(this.border.object);
+    }
+
+    changeValue() {
+        if (!this.value) {
+            this.canvas.font = `${this.size}px Arial`;
+            this.canvas.fillText('1', this.xc - this.size * 0.3, this.yc + this.size * 0.35);
+            this.value = true;
+        } else {
+            this.canvas.clearRect(this.xc - this.size * 0.3, this.yc - this.size * 0.5, this.size * 0.5, this.size);
+            this.value = false;
+        }
+    }
+
+    print(size, x0, y0, lineWidth, type = 'stroke') {
+        let hexagon = {object: new Path2D()};
+        let coordinates = [];
+        let xh, yh;
+        if (type === 'fill') {
+            y0 = y0 + lineWidth;
+            size = size - lineWidth;
+        }
+        this.canvas.beginPath();
+        this.canvas.moveTo(x0, y0);
+        hexagon.object.lineTo(x0, y0);
+        xh = x0;
+        yh = y0;
+        for (let side = 0; side < 6; side++) {
+            coordinates.push({x: xh, y: yh});
+            xh = xh + size * Math.sin(Math.PI / 3 - side * Math.PI / 3);
+            yh = yh + size * Math.cos(Math.PI / 3 - side * Math.PI / 3);
+            hexagon.object.lineTo(xh, yh);
+        }
+        this.canvas.lineWidth = 2;
+        hexagon.coordinates = coordinates;
+        return hexagon;
+    }
+}
---
+++ scripts/index.js
@@ -0,0 +1,53 @@
+import {PrintedArea} from "./PrintedArea.js";
+
+let printedArea;
+let sizesForm = document.forms['sizes'];
+let findDomensButton = document.getElementById('findDomens');
+let autoForm = document.forms['auto'];
+console.log(sizesForm);
+sizesForm.elements['lSize'].value = 3;
+sizesForm.elements['mSize'].value = 5;
+sizesForm.elements['nSize'].value = 7;
+autoForm.elements['probability'].value = 0.5;
+sizesForm.onsubmit = () => {
+    printedArea = new PrintedArea({
+        l: parseInt(sizesForm.elements['lSize'].value),
+        m: parseInt(sizesForm.elements['mSize'].value),
+        n: parseInt(sizesForm.elements['nSize'].value)
+    });
+    return false;
+};
+autoForm.onsubmit = () => {
+    printedArea = new PrintedArea({
+        l: parseInt(sizesForm.elements['lSize'].value),
+        m: parseInt(sizesForm.elements['mSize'].value),
+        n: parseInt(sizesForm.elements['nSize'].value)
+    });
+    printedArea.hexagons.forEach(hg=>{
+        if(Math.random() >= 1-autoForm.elements['probability'].value){
+            hg.changeValue();
+        }
+    });
+    printedArea.findDomens((qd,qnd,qh,qht)=>{
+        let table = document.getElementById('calculations');
+        let rows = table.querySelectorAll('tr.tableRow');
+        let newRow = document.createElement('tr');
+        newRow.classList.add('tableRow');
+        newRow.innerHTML = `
+            <td>${autoForm.elements['probability'].value}</td>
+            <td>${qd}</td>
+            <td>${qnd}</td>
+            <td>${qh}, ${qht}</td>
+        `;
+        table.append(newRow);
+        if(rows.length>9)rows[0].remove();
+    });
+    return false;
+};
+findDomensButton.addEventListener('click', () => {
+    printedArea.findDomens();
+});
+
+
+
+
